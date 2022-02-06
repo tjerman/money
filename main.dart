4f36e5ac16f838cf35b9cf3477eb061ee1000c8a
Tomaž Jerman <tomaz.jerman121@gmail.com>
2022-02-06 14:06:28 +0100

    Rework authentication to work on Planet Crust

--- main.dart
+++ main.dart
@@ -1,7 +1,19 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'screens/LoginScreen.dart';
 import 'screens/TLDRScreen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+// This is me being lazy; don't use this in production and fix certificate issues
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -10,10 +22,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Money',
-
       initialRoute: '/login',
       routes: {
-        '/tldr': (context) {return const TLDRScreen();},
+        '/tldr': (context) {
+          return const TLDRScreen();
+        },
         '/login': (context) => LoginScreen(),
       },
     );
@@ -21,6 +34,9 @@ class MyApp extends StatelessWidget {
 }
 
 Future main() async {
+// This is me being lazy; don't use this in production and fix certificate issues
+  HttpOverrides.global = MyHttpOverrides();
+
   await dotenv.load();
   runApp(const MyApp());
 }
--- screens/LoginScreen.dart
+++ screens/LoginScreen.dart
@@ -1,15 +1,14 @@
+import 'dart:async';
 import 'dart:convert';
 
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
-import 'package:flutter_appauth/flutter_appauth.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import '../widgets/Loader.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 import '../utilities/State.dart';
-
-final FlutterAppAuth _appAuth = FlutterAppAuth();
-TextEditingController _accessTokenTextController = TextEditingController();
-TextEditingController _refreshTokenTextController = TextEditingController();
+import '../utilities/helpers.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -19,6 +18,10 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  late StreamSubscription<String> _wwURLChange;
+  late FlutterWebviewPlugin _ww;
+  late String _stateKey;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,26 +30,180 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   void auth() async {
-    var result = await _appAuth.authorizeAndExchangeCode(
-        AuthorizationTokenRequest(
-            dotenv.env['AUTH_CLIENT_ID'], dotenv.env['AUTH_REDIRECT_URL'],
-            serviceConfiguration: AuthorizationServiceConfiguration(
-                'https://${dotenv.env['API_DOMAIN']}/auth/oauth2/authorize',
-                'https://${dotenv.env['API_DOMAIN']}/auth/oauth2/token'),
-            scopes: ['profile', 'api'],
-            clientSecret: dotenv.env['AUTH_CLIENT_SECRET'],
-            promptValues: ['login']));
+    _wwURLChange = _ww.onUrlChanged.listen(exchangeTokens);
+    initFlow();
+  }
 
-    state.accessToken = _accessTokenTextController.text = result.accessToken;
-    state.refreshToken = _refreshTokenTextController.text = result.refreshToken;
-    state.userID = jsonDecode(
-        utf8.fuse(base64).decode(state.accessToken.split(".")[1]))["sub"]!;
+  void initFlow() {
+    _ww.launch(
+      'https://${dotenv.env['API_DOMAIN']}/auth/oauth2/authorize' +
+          '?response_type=code' +
+          '&client_id=${dotenv.env['AUTH_CLIENT_ID']}' +
+          '&state=$_stateKey' +
+          '&scope=profile+api' +
+          '&redirect_uri=${dotenv.env['AUTH_REDIRECT_URL']}',
+      ignoreSSLErrors: true,
+    );
+  }
 
+  void exchangeTokens(String url) async {
+    final flutterWebviewPlugin = new FlutterWebviewPlugin();
+    if (url.indexOf("oauthredirect") <= -1) {
+      return;
+    }
+
+    flutterWebviewPlugin.close();
+
+    // Get the required params from the url
+    var parsed = parseRedirect(url);
+
+    if (parsed.state != _stateKey) {
+      throw new Exception("request and response states don't match");
+    }
+
+    // Exchange OAuth2 bits
+    var rsp =
+        await http.post('https://${dotenv.env['API_DOMAIN']}/auth/oauth2/token',
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+              'Accept': ' application/json',
+              // This indicates what auth client Corteza should use
+              'Authorization': 'Basic ' +
+                  base64Encode(utf8.encode(
+                      '${dotenv.env['AUTH_CLIENT_ID']}:${dotenv.env['AUTH_CLIENT_SECRET']}')),
+            },
+            encoding: Encoding.getByName('utf-8'),
+            body: {
+              "code": parsed.code,
+              "grant_type": "authorization_code",
+              "redirect_uri": dotenv.env['AUTH_REDIRECT_URL'],
+            });
+
+    // Don't do this in production; have errors make more sense
+    if (rsp.statusCode >= 400) {
+      throw new Exception("something went wrong");
+    }
+
+    var c = Creds.fromJson(jsonDecode(rsp.body));
+    state.accessToken = c.accessToken;
+    state.refreshToken = c.refreshToken;
+    state.userID = c.userID;
+
+    dispose();
     Navigator.pushReplacementNamed(context, '/tldr');
+  }
+
+  void dispose() {
+    _wwURLChange.cancel();
+    FlutterWebviewPlugin().dispose();
+    super.dispose();
+  }
+
+  RDRUrl parseRedirect(String url) {
+    String code = "";
+    String state = "";
+
+    var r = RegExp(r'error=([\w]+)');
+    if (r.hasMatch(url)) {
+      var m = r.firstMatch(url);
+      throw new Exception(m!.group(1));
+    }
+
+    r = RegExp(r'code=([\w]+)');
+    if (!r.hasMatch(url)) {
+      throw new Exception("missing expected code parameter");
+    }
+    var m = r.firstMatch(url);
+    code = m!.group(1)!;
+
+    r = RegExp(r'state=([\w]+)');
+    if (!r.hasMatch(url)) {
+      throw new Exception("missing expected state parameter");
+    }
+    m = r.firstMatch(url);
+    state = m!.group(1)!;
+
+    return RDRUrl(code, state);
   }
 
   void initState() {
     super.initState();
+    _ww = FlutterWebviewPlugin();
+    // 34 is what Corteza uses so might as well
+    _stateKey = getRandomString(34);
     auth();
   }
+}
+
+class Creds {
+  String accessToken = "";
+  String email = "";
+  int expiresIn = 0;
+  String handle = "";
+  String name = "";
+  String refreshToken = "";
+  int refreshTokenExpiresIn = 0;
+  String roles = "";
+  String scope = "";
+  String userID = "";
+  String tokenType = "";
+
+  Creds();
+
+  factory Creds.fromJson(Map<String, dynamic> json) {
+    var out = Creds();
+
+    if (json.containsKey('access_token')) {
+      out.accessToken = json['access_token'];
+    }
+    if (json.containsKey('email')) {
+      out.email = json['email'];
+    }
+    if (json.containsKey('expires_in')) {
+      out.expiresIn = json['expires_in'];
+    }
+    if (json.containsKey('handle')) {
+      out.handle = json['handle'];
+    }
+    if (json.containsKey('name')) {
+      out.name = json['name'];
+    }
+    if (json.containsKey('refresh_token')) {
+      out.refreshToken = json['refresh_token'];
+    }
+    if (json.containsKey('refresh_token_expires_in')) {
+      out.refreshTokenExpiresIn = json['refresh_token_expires_in'];
+    }
+    if (json.containsKey('roles')) {
+      out.roles = json['roles'];
+    }
+    if (json.containsKey('scope')) {
+      out.scope = json['scope'];
+    }
+    if (json.containsKey('sub')) {
+      out.userID = json['sub'];
+    }
+    if (json.containsKey('token_type')) {
+      out.tokenType = json['token_type'];
+    }
+
+    if (out.accessToken == "") {
+      throw new Exception('accessToken not provided');
+    }
+    if (out.refreshToken == "") {
+      throw new Exception('refreshToken not provided');
+    }
+    if (out.userID == "" || out.userID == "0") {
+      throw new Exception('userID not provided');
+    }
+
+    return out;
+  }
+}
+
+class RDRUrl {
+  String code;
+  String state;
+
+  RDRUrl(this.code, this.state);
 }
